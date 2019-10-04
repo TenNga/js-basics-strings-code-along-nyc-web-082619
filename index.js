2fc5e0733e3da2758cccf6516910117706fc25c5
Kevin Karma <kevinkarma55@gmail.com>
2019-10-04 23:33:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 // Write your code in this file!
 const currentUser = "Grace hopper";
-const welcomeMessage = "Welcome to Flatbook, " + currentUser;
+const welcomeMessage = "Welcome to Flatbook, " + currentUser+"!";
